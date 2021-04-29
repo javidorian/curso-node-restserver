fcbe3d546d236ba8f13b1afccebc2904810905c0
javidorian <javidorian@gmail.com>
2021-04-29 13:40:56 +0200

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,50 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre, apikey } = req.query;
+    res.json({
+        msg: "get API - controlador",
+        query
+    });
+};
+
+const usuariosPost = (req, res = response) => {
+
+    const body = req.body;
+    res.json({
+        msg: "post API - controlador",
+        body: body
+    });
+};
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+    res.json({
+        msg: "put API - controlador",
+        id
+    });
+};
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: "delete API - controlador"
+    });
+};
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: "patch API - controlador"
+    });
+};
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete,
+    usuariosPatch
+}
